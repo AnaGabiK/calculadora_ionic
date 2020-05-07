7026eb169f4dbc611acf417b8e266b5f822b75d7
Ana Gabrielle Kataoka <ananinha5660@gmail.com>
2020-05-07 15:54:08 -0300

    calculadora

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+export class HomePage {
+
+  valor: number = 0;
+  memoria: number = 0;
+  operacao: string = "";
+  ponto: string = "";
+
+  constructor() {}
+
+  clickBotao(parametro: string) {
+    this.valor = Number("" + this.valor + this.ponto + parametro);
+    this.ponto = "";
+  }
+
+  clickOutros(parametro: string) {
+    if (parametro == "+/-") {
+      this.valor = (this.valor * -1)
+    } else if (parametro == ".") {
+      if(!(String(this.valor).indexOf(".")>0)) {
+        this.ponto = ".";
+      }
+    } else if (parametro == "C") {
+      this.valor = 0;
+      this.memoria = 0;
+      this.operacao = "";
+      this.ponto = "";
+    }
+  }
+
+  calcula(tipo:string) {
+    if (this.operacao == "") {
+      this.memoria = this.valor;
+    }
+    else if (this.operacao == "+") {
+      this.memoria = this.memoria + this.valor;
+    }
+    else if (this.operacao == "-") {
+      this.memoria = this.memoria - this.valor;
+    }
+    else if (this.operacao == "*") {
+      this.memoria = this.memoria * this.valor;
+    }
+    else if (this.operacao == "/") {
+      this.memoria = this.memoria / this.valor;     
+    }
+    else if(tipo == "=") {
+      this.valor = this.memoria;
+      this.memoria = 0;
+      this.operacao = "";
+    }
+    this.operacao = tipo;
+    this.valor = 0;    
+  }
+}
